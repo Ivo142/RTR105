1c3cf88be704fff7f4de9ef3d6255dfff95e2580
user <ivo.kristofers.gadiskis@gmail.com>
2018-10-02 13:35:29 +0300

    20181002_13_31

---
+++ dgr_20181002.py
@@ -0,0 +1,204 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(459)
+459
+>>> print(Fate is a lie
+      
+SyntaxError: invalid syntax
+>>> print(earth is flat)
+
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(earth is flat)
+NameError: name 'earth' is not defined
+>>> print('ērath is flat'°
+      
+SyntaxError: invalid syntax
+>>> print('ēart is flat')
+ēart is flat
+>>> print(raise global )
+SyntaxError: invalid syntax
+>>> x= 4
+>>> y= 2
+>>> z= x + y
+>>> p= z^2
+>>> x
+4
+>>> y
+2
+>>> z
+6
+>>> p
+4
+>>> p= z * z
+>>> p
+36
+>>> k= p^2 - zx + yx
+
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    k= p^2 - zx + yx
+NameError: name 'zx' is not defined
+>>> k= p^2 - z*x + y*x
+>>> k
+-42
+>>> k=p**2 - z*x +y*x
+>>> k
+1280
+>>> k=p^2
+>>> k
+38
+>>> typ (k)\
+
+    
+
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    typ (k)\
+NameError: name 'typ' is not defined
+>>> type(k)
+<type 'int'>
+>>> type(earth)
+
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    type(earth)
+NameError: name 'earth' is not defined
+>>> minion=bananna
+
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    minion=bananna
+NameError: name 'bananna' is not defined
+>>> bannana='fruity  fruit'
+>>> minion=bannana**2
+
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    minion=bannana**2
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> minion=bananna*2
+
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    minion=bananna*2
+NameError: name 'bananna' is not defined
+>>> minion=bannana
+>>> minion
+'fruity  fruit'
+>>> type(minion)
+<type 'str'>
+>>> type(1)
+<type 'int'>
+>>> type(6.9)
+<type 'float'>
+>>> print(float(602)+69)
+671.0
+>>> i=459
+>>> type(i)
+<type 'int'>
+>>> f=float(i)
+>>> print(i)
+459
+>>> print(f)
+459.0
+>>> type(f)
+<type 'float'>
+>>> print(7/3
+
+
+
+
+
+      )
+2
+>>> jeff= 4
+>>> type(jeff)
+<type 'int'>
+>>> print(jeff**2
+      )
+16
+>>> print(jeff +1
+      )
+5
+>>> bob='33'
+>>> type(bob)
+<type 'str'>
+>>> print(bob + jeff)
+
+Traceback (most recent call last):
+  File "<pyshell#58>", line 1, in <module>
+    print(bob + jeff)
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> print(bob + 1
+      )
+
+Traceback (most recent call last):
+  File "<pyshell#60>", line 1, in <module>
+    print(bob + 1
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> nam=input('Who are you? ')
+Who are you? print('Welcome' nam)
+
+Traceback (most recent call last):
+  File "<pyshell#61>", line 1, in <module>
+    nam=input('Who are you? ')
+  File "<string>", line 1
+    print('Welcome' nam)
+        ^
+SyntaxError: invalid syntax
+>>> nam=input('Who are you?')
+Who are you? Ivo
+
+Traceback (most recent call last):
+  File "<pyshell#62>", line 1, in <module>
+    nam=input('Who are you?')
+  File "<string>", line 1, in <module>
+NameError: name 'Ivo' is not defined
+>>> 
+>>> 
+>>> Hours=input('how many hours?')
+how many hours?35
+>>> hopurs
+
+Traceback (most recent call last):
+  File "<pyshell#66>", line 1, in <module>
+    hopurs
+NameError: name 'hopurs' is not defined
+>>> hours
+
+Traceback (most recent call last):
+  File "<pyshell#67>", line 1, in <module>
+    hours
+NameError: name 'hours' is not defined
+>>> Hours
+35
+>>> Rate=input('What is rate?')
+What is rate?2.75
+>>> Rate
+2.75
+>>> Pay=Hours * Rate
+>>> pay
+
+Traceback (most recent call last):
+  File "<pyshell#72>", line 1, in <module>
+    pay
+NameError: name 'pay' is not defined
+>>> Pay
+96.25
+>>> Paycheck=print(Hours) print(Rate) print(Pay)
+SyntaxError: invalid syntax
+>>> Paycheck= print(Hours) print(Rate) print(Pay)
+SyntaxError: invalid syntax
+>>> Paycheck=(Hours) print(Rate) print(Pay)
+SyntaxError: invalid syntax
+>>> Paycheck=print(Hours)*print(Rate)*print(Pay)
+SyntaxError: invalid syntax
+>>> Pay
+96.25
+>>> Rate
+2.75
+>>> Hours
+35
+>>> 
