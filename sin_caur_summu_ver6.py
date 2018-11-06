e9a97dcaa8899eba6f84d8da9426df713f5ff072
user <ivo.kristofers.gadiskis@gmail.com>
2018-11-06 13:56:44 +0200

    20181002_13_31

---
+++ sin_caur_summu_ver6.py
@@ -0,0 +1,17 @@
+from math import cos
+
+
+def cos(x/2):
+    k = 0
+    a = (-1)*x**2/(8)
+    S = a
+    print ("izdruka no liet.f. a0 = %6.2f"%(a,S)
+
+           while k < 500
+           k = k + 1
+           R = 
+    
+    
+
+
+
